8326196d5f1c6d0b8b4dd58e6ff446c65555a02d
Jennifer Lopez <jenniferlopezh0@gmail.com>
2023-09-02 13:47:16 -0700

    broke down function and created tests for getTeamPointsAndGoalDifferential()

--- TableRankings.js
+++ TableRankings.js
@@ -1,109 +1,132 @@
-$(document).ready(function(){ // comment out to run tests |  uncomment to use on browser
-const TableRankings = function(){
-    var loadDomListners = function() {
-     $(".restart-container").off("click", ".clear");
-     $(".restart-container").on("click", ".clear" , clearLocalStorage);
+// $(document).ready(function(){ // comment out to run tests |  uncomment to use on browser
+const TableRankings = function () {
+    var loadDomListners = function () {
+        $(".restart-container").off("click", ".clear");
+        $(".restart-container").on("click", ".clear", clearLocalStorage);
     };
 
-    var startOrganizeTeams = function(){
-        // your logic starts here, button 
+    // Calculate points & goal differencial for all teams
+    var getTeamPointsAndGoalDifferential = function (teamData, matchData) {
+
+        if (!teamData && !matchData) {
+            return "Please enter data";
+        }
+        if (!teamData) {
+            return "Please enter team data";
+        }
+        if (!matchData) {
+            return "Please enter match data";
+        }
 
         var finalTeamPoints = [];
 
         // sets up to calculate final point & goal differencial
-        getTeamsData().forEach(indTeam => {
+        teamData.forEach(indTeam => {
             let team_id = indTeam.team_id;
-            let newTeam = {team_id: team_id, points: 0, gd: 0};
+            let newTeam = { team_id: team_id, points: 0, gd: 0 };
             finalTeamPoints.push(newTeam);
         })
 
-        // Calculate points & goal differencial for all teams
-        getMatchHistoryData().forEach(indMatch => {
-            
+        matchData.forEach(indMatch => {
+
             // Calculate goal differencial
             let team_one_goal_count = indMatch.team_one_goal_count;
             let team_two_goal_count = indMatch.team_two_goal_count;
-            
-            let team_one_gd = team_one_goal_count - team_two_goal_count; 
+
+            let team_one_gd = team_one_goal_count - team_two_goal_count;
             let team_two_gd = team_two_goal_count - team_one_goal_count;
 
             // Place them
             let indexTeam1inFinal = findInFinal(finalTeamPoints, indMatch.team_one_id);
             let indexTeam2inFinal = findInFinal(finalTeamPoints, indMatch.team_two_id);
-            
+
             finalTeamPoints[indexTeam1inFinal].gd += team_one_gd;
             finalTeamPoints[indexTeam2inFinal].gd += team_two_gd;
 
             // Calculate + place points
-            if(indMatch.tie) {
+            if (indMatch.tie) {
                 finalTeamPoints[indexTeam1inFinal].points++;
                 finalTeamPoints[indexTeam2inFinal].points++;
-                
+
             }
             else {
                 let indexInFinalArrayWinner = findInFinal(finalTeamPoints, indMatch.winner_id);
-                finalTeamPoints[indexInFinalArrayWinner].points+=3;
+                finalTeamPoints[indexInFinalArrayWinner].points += 3;
             }
 
-        })        
+        })
 
-        finalTeamPoints.sort((team_a, team_b) => team_a.gd - team_b.gd);
+        return finalTeamPoints;
+    }
 
-        // Similar gd?
-        var i = 0; 
-        var j = 1;
+    // new function that returns similar points getTiedTEams()
 
-        // while(j != finalTeamPoints().length) {
-            let val1 = finalTeamPoints[i];
-            let val2 = finalTeamPoints[j];
+    var rankTeams = function (calculatedTeamData, matchData) {
+        calculatedTeamData.sort((team_a, team_b) => team_b.points - team_a.points);
 
-            // console.log(val1.points, val2.points)
+        for (let i = 0; i < calculatedTeamData.length - 1; i++) {
+            let firstTeam = calculatedTeamData[i];
+            let secondTeam = calculatedTeamData[i + 1];
 
-            if(val1.points == val2.points) {
-                // look at gd
+            if (firstTeam.points == secondTeam.points) {
+                // getMatchHistoryData.forEach(function(match){
+                //     let team_one_id = match.team_one_id;
+                //     let team_two_id = match.team_two_id;
+
+                //     let match1 = firstTeam.team_id == team_one_id && secondTeam.team_id == team_two_id;
+                //     let match2 = secondTeam.team_id == team_one_id && firstTeam.team_id == team_two_id;
+                //     if(match1 || match2) {
+                //         return match.winner_id;
+                //     }
+                // })
+
+                // let calculatedTeamDataLoserIndex = findInFinal(calculatedTeamData, )
+
+                // if(winner_id == secondTeam.team_id) {
+                //     // [a, b] = [b, a];
+                //     let loserObject = calculatedTeamData
+                //     []
+                // }
             }
-            else if(val1.points > val2.points) {
-                // do something else
-            }
-        // }
+        }
 
 
         // dataReadyForLocalStorage([{ name:"Team Name", points: 12 }]); // comment out when run test | uncomment for browser
     };
 
-    var findInFinal = function(finalTeamPoints, team_id) {
+    var findInFinal = function (finalTeamPoints, team_id) {
         return finalTeamPoints.findIndex(obj => obj.team_id === team_id);
     }
 
     // do not change this, this send data to the front end 
     // CALL IT WHEN YOU GOT YOUR SOLUTION 
-    var dataReadyForLocalStorage = function(rankedTeams){
+    var dataReadyForLocalStorage = function (rankedTeams) {
         localStorage.setItem("teams", JSON.stringify(rankedTeams));
     };
-    
+
     // do not change this, clears tour local storage
-    var clearLocalStorage = function() {
+    var clearLocalStorage = function () {
         localStorage.clear();
     };
 
     // no need to update the funciton.
     // call it, retruns array of objects of Soccer Teams
-    var getTeamsData = function() { 
+    var getTeamsData = function () {
         return [
             {
-                team_id: 1, 
+                team_id: 1,
                 team_name: "Real Madrid FC",
             },
             {
-                team_id: 2, 
+                team_id: 2,
                 team_name: "FC Barcelona",
             },
             {
-                team_id: 3, 
+                team_id: 3,
                 team_name: "Chealse FC",
             },
             {
-                team_id: 4, 
+                team_id: 4,
                 team_name: "Matchester City FC",
             },
         ]
@@ -111,64 +134,64 @@ const TableRankings = function(){
 
     // no need to update the funciton.
     // call it, retruns array of objects of matches between the teams
-    var getMatchHistoryData = function() { 
+    var getMatchHistoryData = function () {
         return [
             {
                 match_id: 100,
-                team_one_id: 1, 
+                team_one_id: 1,
                 team_two_id: 2,
-                team_one_goal_count:2,
-                team_two_goal_count:3,
-                winner_id:2,
-                losser_id:1,
+                team_one_goal_count: 2,
+                team_two_goal_count: 3,
+                winner_id: 2,
+                losser_id: 1,
                 tie: false,
             },
             {
                 match_id: 445,
-                team_one_id: 3, 
+                team_one_id: 3,
                 team_two_id: 2,
-                team_one_goal_count:2,
-                team_two_goal_count:3,
-                winner_id:2,
-                losser_id:3,
+                team_one_goal_count: 2,
+                team_two_goal_count: 3,
+                winner_id: 2,
+                losser_id: 3,
                 tie: false,
             },
             {
                 match_id: 325,
-                team_one_id: 4, 
+                team_one_id: 4,
                 team_two_id: 2,
-                team_one_goal_count:2,
-                team_two_goal_count:2,
+                team_one_goal_count: 2,
+                team_two_goal_count: 2,
                 winner_id: null,
-                losser_id:null,
+                losser_id: null,
                 tie: true,
             },
             {
                 match_id: 605,
-                team_one_id: 1, 
+                team_one_id: 1,
                 team_two_id: 3,
-                team_one_goal_count:1,
-                team_two_goal_count:1,
+                team_one_goal_count: 1,
+                team_two_goal_count: 1,
                 winner_id: null,
                 losser_id: null,
                 tie: true,
             },
             {
                 match_id: 195,
-                team_one_id: 4, 
+                team_one_id: 4,
                 team_two_id: 1,
-                team_one_goal_count:2,
-                team_two_goal_count:1,
+                team_one_goal_count: 2,
+                team_two_goal_count: 1,
                 winner_id: 4,
                 losser_id: 1,
                 tie: false,
             },
             {
                 match_id: 812,
-                team_one_id: 3, 
+                team_one_id: 3,
                 team_two_id: 4,
-                team_one_goal_count:3,
-                team_two_goal_count:2,
+                team_one_goal_count: 3,
+                team_two_goal_count: 2,
                 winner_id: 3,
                 losser_id: 4,
                 tie: false,
@@ -176,21 +199,24 @@ const TableRankings = function(){
 
         ]
     };
- startOrganizeTeams();
-//  loadDomListners(); // comment out to run tests
+    // startOrganizeTeams();
+    //  loadDomListners(); // comment out to run tests
 
-   // add function to return statment
-   // this will give you access to your functions outside this file
-   // nameOfJsFile.myFunction(), 
-   // like for testing, youll need access to your fucntion in testing file
+    // add function to return statment
+    // this will give you access to your functions outside this file
+    // nameOfJsFile.myFunction(), 
+    // like for testing, youll need access to your fucntion in testing file
     return {
         dataReadyForLocalStorage,
-        getTeamsData, 
+        getTeamsData,
         getMatchHistoryData,
+        // startOrganizeTeams,
+        getTeamPointsAndGoalDifferential,
+        rankTeams
     }
 }
 
 TableRankings();
 module.exports = TableRankings();
-window.TableRankings = TableRankings(); // comment out to run tests | uncomment to use on browser
-}); // comment out to run tests | uncomment to use on browser
+// window.TableRankings = TableRankings(); // comment out to run tests | uncomment to use on browser
+// }); // comment out to run tests | uncomment to use on browser
--- TableRankings.test.js
+++ TableRankings.test.js
@@ -2,8 +2,8 @@ const TableRankings = require("./TableRankings");
 
 // use these tests as 'guides': description block for your function
 // which a describe block contain holds it blocks for test cases
-describe("getTeamsData()", function() {
-    it("when invoked it returns an array of objects, which are soccer teams ", function(){
+describe("getTeamsData()", function () {
+    it("when invoked it returns an array of objects, which are soccer teams ", function () {
 
         const soccerTeams = TableRankings.getTeamsData();
         expect(soccerTeams[0].team_name).toBe("Real Madrid FC");
@@ -13,12 +13,47 @@ describe("getTeamsData()", function() {
     })
 });
 
-describe("getTeamsData()", function() {
-    it("when invoked it returns an array of objects, which are match histroy", function(){
+describe("getTeamsData()", function () {
+    it("when invoked it returns an array of objects, which are match histroy", function () {
         const matches = TableRankings.getMatchHistoryData();
         // there are 6 games in between these 4 teams, each played 3 games
         expect(matches.length).toBe(6);
-    
+
     })
 });
 // your tests here 
+describe("getTeamPointsAndGoalDifferential()", function () {
+    it("given match data & team data, it returns array with each team's final point & goal differencial", function () {
+        const teamData = TableRankings.getTeamsData();
+        const matchData = TableRankings.getMatchHistoryData();
+        const result = TableRankings.getTeamPointsAndGoalDifferential(teamData, matchData);
+        expect(result).toStrictEqual([{ "gd": -2, "points": 1, "team_id": 1 }, { "gd": 2, "points": 7, "team_id": 2 }, { "gd": 0, "points": 4, "team_id": 3 }, { "gd": 0, "points": 4, "team_id": 4 }]);
+    })
+    it("given match data & no team data, it asks user to enter team data", function () {
+        const teamData = undefined;
+        const matchData = TableRankings.getMatchHistoryData();
+        const result = TableRankings.getTeamPointsAndGoalDifferential(teamData, matchData);
+        expect(result).toBe("Please enter team data");
+    })
+    it("given team data & no match data, it asks user to enter match data", function () {
+        const teamData = TableRankings.getTeamsData();
+        const matchData = undefined;
+        const result = TableRankings.getTeamPointsAndGoalDifferential(teamData, matchData);
+        expect(result).toBe("Please enter match data");
+    })
+    it("given no data, it asks user to enter all data", function () {
+        const teamData = undefined;
+        const matchData = undefined;
+        const result = TableRankings.getTeamPointsAndGoalDifferential(teamData, matchData);
+        expect(result).toBe("Please enter data");
+    })
+})
+
+describe("rankTeams()", function () {
+    it("given the calculated team data & match data, it returns a ranked array of the teams ", function () {
+        const calculatedTeamData = [{ "gd": -2, "points": 1, "team_id": 1 }, { "gd": 2, "points": 7, "team_id": 2 }, { "gd": 0, "points": 4, "team_id": 3 }, { "gd": 0, "points": 4, "team_id": 4 }];
+        const matchData = TableRankings.getMatchHistoryData();
+        const result = TableRankings.rankTeams(calculatedTeamData, matchData);
+        expect(result).toStrictEqual("hallo");
+    })
+})
